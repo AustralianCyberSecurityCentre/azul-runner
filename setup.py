a99c921699f1e77f1ac02c46f8bb5b55ef4cbedd
dan-acsc <130635354+dan-acsc@users.noreply.github.com>
2025-12-17 11:35:34 +1100

    feat: add long description for pypi. (#7)
    feat: add long description for pypi.
    
    Co-authored-by: dan-acsc <dan-acsc@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ import os
 
 from setuptools import setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 
 def open_file(fname):
     """Open and return a file-like object for the relative filename."""
@@ -18,8 +21,20 @@ setup(
     url="https://www.asd.gov.au/",
     packages=["azul_runner"],
     include_package_data=True,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries",
+    ],
     python_requires=">=3.12",
-    classifiers=[],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     use_scm_version=True,
     setup_requires=["setuptools_scm"],
     install_requires=[r.strip() for r in open_file("requirements.txt") if not r.startswith("#")],
