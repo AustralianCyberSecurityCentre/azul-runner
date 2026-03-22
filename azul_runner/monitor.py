2c5f0f77fb9450dff0d3411982799eb3c990a1b8
kevin-acsc <265393893+kevin-acsc@users.noreply.github.com>
2026-03-22 23:13:55 +0000

    prevent runner form deleting our gitconfig tmpfiles

--- azul_runner/monitor.py
+++ azul_runner/monitor.py
@@ -276,6 +276,8 @@ class GitSync:
         self.clone_depth: int = clone_depth
         self.submodules: str = submodules
 
+        self._gitconfig = tempfile.NamedTemporaryFile(delete=False, prefix=".git").name
+        self._gitcredential = tempfile.NamedTemporaryFile(delete=False, prefix=".git").name
         self._sync_failures = 0
         self._notify_thread: threading.Thread = threading.Thread(target=self._run_loop)
         self._stop_event: threading.Event = threading.Event()
@@ -315,12 +317,13 @@ class GitSync:
     def pull(self):
         """Fetch updates from the remote, if available."""
         logger.info(f"Pulling updates from remote repository at {self.watch_path}")
+        self._refresh_auth()
         if not self.do_ssh_auth and (self.username or self.password):
             # Refresh creds in memory since we are using cache as the storage mechanism
             self._refresh_https_auth()
 
         pull_cmd = ["pull", "origin", "--verbose", "--no-progress", "--prune"]
-        self._run_git(pull_cmd)
+        self._run_git(pull_cmd, refresh_auth=True)
         self._sync_failures = 0
 
         if self._update_event.is_set():
@@ -342,14 +345,7 @@ class GitSync:
 
         # ~/.gitconfig may not be writable
         if not os.access(os.path.expanduser("~"), os.W_OK):
-            os.environ["GIT_CONFIG_GLOBAL"] = tempfile.NamedTemporaryFile(delete=False).name
-
-        if not self.do_ssh_auth and (self.username or self.password):
-            # Refresh creds in memory since we are using cache as the storage mechanism
-            self._refresh_https_auth()
-        if self.do_ssh_auth:
-            self._run_git(["config", "--global", "core.sshCommand", f"ssh -i {self.ssh_key_path}"])
-            logger.info(f"Configured SSH authentication for git repository at {self.repo}")
+            os.environ["GIT_CONFIG_GLOBAL"] = self._gitconfig
 
         if not os.path.exists(os.path.join(self.watch_path, ".git")):
             # clone if repo does not exist
@@ -359,20 +355,22 @@ class GitSync:
                 clone_cmd.insert(2, f"--branch={self.branch}")
             if self.clone_depth > 0:
                 clone_cmd.insert(2, f"--depth={self.clone_depth}")
-            self._run_git(clone_cmd)
+            self._run_git(clone_cmd, refresh_auth=True)
 
-        if self.submodules != "off":
-            submodule_cmd = ["submodule", "update", "--init"]
-            if self.submodules == "recursive":
-                submodule_cmd.append("--recursive")
-            if self.clone_depth > 0:
-                submodule_cmd.append(f"--depth={self.clone_depth}")
-            self._run_git(submodule_cmd)
-            logger.info(f"Initialized submodules with option {self.submodules}")
+            if self.submodules != "off":
+                submodule_cmd = ["submodule", "update", "--init"]
+                if self.submodules == "recursive":
+                    submodule_cmd.append("--recursive")
+                if self.clone_depth > 0:
+                    submodule_cmd.append(f"--depth={self.clone_depth}")
+                self._run_git(submodule_cmd)
+                logger.info(f"Initialized submodules with option {self.submodules}")
 
-    def _run_git(self, cmd: list[str], input: str = None) -> str:
+    def _run_git(self, cmd: list[str], input: str = None, refresh_auth=False) -> str:
         """Run a git command in the watch path and return the output."""
         try:
+            if refresh_auth:
+                self._refresh_auth()
             return subprocess.check_output(  # noqa: S603
                 ["git"] + cmd,
                 cwd=self.watch_path,
@@ -384,26 +382,31 @@ class GitSync:
             logger.error(msg)
             raise GitError(msg) from e
 
-    def _refresh_https_auth(self):
-        logger.info("Refreshing HTTPS authentication for git")
-        cmd = ["config", "--global", "credential.helper"]
-        # home directory may not be writable, but cache needs home directory to be writable
-        if os.access(os.path.expanduser("~"), os.W_OK):
-            cmd += ["cache"]
-        else:
-            cmd += [f"store --file={tempfile.NamedTemporaryFile(delete=False).name}"]
+    def _refresh_auth(self):
+        if not self.do_ssh_auth and (self.username or self.password):
+            # Refresh creds in memory since we are using cache as the storage mechanism
+            logger.info("Refreshing HTTPS authentication for git")
+            cmd = ["config", "--global", "credential.helper"]
+            # home directory may not be writable, but cache needs home directory to be writable
+            if os.access(os.path.expanduser("~"), os.W_OK):
+                cmd += ["cache"]
+            else:
+                cmd += [f"store --file={self._gitcredential}"]
 
-        self._run_git(cmd)
-        if "@" in self.repo:
-            # if @ is in repo url, it contains a username and we need to use it instead of self.username
-            input = f"url={self.repo}\npassword={self.password}\n"
-        else:
-            # if @ is not in repo url, we can use self.username
-            input = f"url={self.repo}\nusername={self.username}\npassword={self.password}\n"
-        self._run_git(
-            ["credential", "approve"],
-            input=input,
-        )
+            self._run_git(cmd)
+            if "@" in self.repo:
+                # if @ is in repo url, it contains a username and we need to use it instead of self.username
+                input = f"url={self.repo}\npassword={self.password}\n"
+            else:
+                # if @ is not in repo url, we can use self.username
+                input = f"url={self.repo}\nusername={self.username}\npassword={self.password}\n"
+            self._run_git(
+                ["credential", "approve"],
+                input=input,
+            )
+        if self.do_ssh_auth:
+            logger.info(f"Refreshing SSH authentication for git repository at {self.repo}")
+            self._run_git(["config", "--global", "core.sshCommand", f"ssh -i {self.ssh_key_path}"])
 
     def _run_loop(self):
         while not self._stop_event.is_set():
@@ -411,7 +414,7 @@ class GitSync:
                 local = self._run_git(["rev-parse", "HEAD"]).strip()
 
                 ls_cmd = ["ls-remote", "origin"] + ([self.branch] if self.branch else ["HEAD"])
-                remote = self._run_git(ls_cmd).split()[0].strip()
+                remote = self._run_git(ls_cmd, refresh_auth=True).split()[0].strip()
 
                 # post to self.update_event if they are not equal (parent proc now knows to pull then restart the plugin)
                 if local != remote:
@@ -424,6 +427,9 @@ class GitSync:
             except GitError as e:
                 logger.error(f"Error checking for updates from remote repo: {e}")
                 self._sync_failures += 1
+                if self._sync_failures > self.max_sync_failures:
+                    logger.error("Maximum sync failures reached; terminating watch thread.")
+                    break
 
 
 class Monitor:
