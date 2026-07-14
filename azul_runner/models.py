e3e0859eb6f5344c954be16379ecdde62f5da49e
kevin-acsc <265393893+kevin-acsc@users.noreply.github.com>
2026-07-14 05:28:04 +0000

    feat: enable ty

--- azul_runner/models.py
+++ azul_runner/models.py
@@ -545,12 +545,14 @@ class Job(BaseModelStrict):
                     )
                 )
 
-    def get_data(self, label: azm.DataLabel = azm.DataLabel.CONTENT) -> storage.StorageProxyFile | None:
+    def get_data(self, label: azm.DataLabel = azm.DataLabel.CONTENT) -> storage.StorageProxyFile:
         """Return and ensure only one stream with label."""
         streams = self.get_all_data(label)
         if len(streams) > 1:
             raise ValueError(f'more than one "{label}" stream for entity {self.id}')
-        return streams[0] if streams else None
+        if not streams:
+            raise ValueError(f'no "{label}" streams for entity {self.id}')
+        return streams[0]
 
     def get_all_data(
         self, label: azm.DataLabel | None = None, file_format: str | None = None
